fd41572076a7d7615e78f7f9dc746d8af52bce38
EthanStanbury <ethan.1998@live.com.au>
2018-04-11 14:43:57 +1000

    Uploading where the intergration is at

--- Application/app/src/main/java/com/example/mischa/pixelbotui/Intergration/SwarmAdapter.java
+++ Application/app/src/main/java/com/example/mischa/pixelbotui/Intergration/SwarmAdapter.java
@@ -1,8 +1,21 @@
 package com.example.mischa.pixelbotui.Intergration;
 
+import com.example.mischa.pixelbotui.Swarm.Swarm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by Ethan on 10/04/2018.
  */
 
 public class SwarmAdapter {
+
+    public static ArrayList<Swarm> WholeSwarm = new ArrayList<>();
+
+    public static void SwarmCreate(HashMap<Integer, Integer> intialSwarm){
+        Swarm add = new Swarm(intialSwarm);
+        WholeSwarm.add(add)
+
+    }
 }
---
+++ Application/app/src/main/java/com/example/mischa/pixelbotui/Intergration/UIAdapter.java
@@ -0,0 +1,56 @@
+package com.example.mischa.pixelbotui.Intergration;
+
+import android.graphics.Color;
+
+import com.example.mischa.pixelbotui.Swarm.Grid;
+import com.example.mischa.pixelbotui.UI.LayoutItem;
+import com.example.mischa.pixelbotui.UI.MainActivity;
+import com.example.mischa.pixelbotui.UI.PBCanvas;
+import com.example.mischa.pixelbotui.UI.Pixel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Ethan on 10/04/2018.
+ */
+
+public class UIAdapter {
+
+    public Grid destinationGrid = new Grid(PBCanvas.xDimension, PBCanvas.yDimension);
+
+
+
+    public void createGridWpixel(){
+
+        for (Pixel p : PBCanvas.uiGrid) {
+            if(p.colour != Color.TRANSPARENT){
+                destinationGrid.addDestination(p);
+            }
+        }
+
+    }
+
+     public HashMap<Integer, Integer> CreateGridBots(){
+        return PBCanvas.BotAmounts;
+    }
+
+//    private ArrayList<ArrayList<LayoutItem>> GetuiGrid(){
+//
+//        ArrayList<ArrayList<LayoutItem>> returnArray = new ArrayList<>();
+//
+//            for (int i = 0; i < PBCanvas.xDimension; i++){
+//                ArrayList<LayoutItem> add = new ArrayList<>();
+//                int index = i;
+//                for(int x = 0; x < PBCanvas.yDimension; x++){
+//                    add.add(PBCanvas.uiGrid[index]);
+//                    index += PBCanvas.xDimension;
+//                }
+//                returnArray.add(add);
+//            }
+//
+//        return  returnArray;
+//    }
+
+
+}
