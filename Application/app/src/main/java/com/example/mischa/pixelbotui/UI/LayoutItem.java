aa391e28beb4a7f8e3cbff2ba64248608103b88b
MischaRippon <mischarippon@gmail.com>
2018-04-11 12:48:53 +1000

    Added new LayoutItem

---
+++ Application/app/src/main/java/com/example/mischa/pixelbotui/UI/LayoutItem.java
@@ -0,0 +1,18 @@
+package com.example.mischa.pixelbotui.UI;
+
+import android.graphics.Rect;
+
+/**
+ * Created by User on 11/04/2018.
+ */
+
+public class LayoutItem {
+
+    int colour;
+    Rect rect;
+
+    public LayoutItem (int colour) {
+        this.colour = colour;
+        this.rect = new Rect();
+    }
+}
